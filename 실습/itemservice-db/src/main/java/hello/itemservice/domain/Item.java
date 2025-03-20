a09919ac194f289fc7c878029f6f29bab58e7628
limlimjo <skyblue.777@daum.net>
2025-03-20 20:31:20 +0900

    [add] 데이터 접근 기술-JPA(JPA 적용1-개발) 실습

--- 실습/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ 실습/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,17 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity // JPA가 사용하는 객체라는 뜻 (이 어노테이션이 있어야 JPA가 인식할 수 있음)
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
